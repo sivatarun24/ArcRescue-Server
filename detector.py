5b16a160245a4574196f9d6f773f31442de93225
Siva Tarun Reddy Ambala <sambala@esri.com>
2025-07-25 20:38:43 -0700

    adding detector and receiver

---
+++ detector.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8x.pt")  # other models yolov8s.pt, yolov8n.pt, yolov8x.pt etc.
+
+def detect_persons(frame):
+    results = model(frame)[0]
+    persons = []
+
+    for box, cls, conf in zip(results.boxes.xyxy, results.boxes.cls, results.boxes.conf):
+        if int(cls.item()) == 0:  # class 0 = person
+            x1, y1, x2, y2 = map(int, box)
+            persons.append((x1, y1, x2, y2, float(conf)))
+
+    return persons
