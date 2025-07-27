b6cd8c2e1a852455135567fdbab9eb0091aec916
Siva Tarun Reddy Ambala <sambala@esri.com>
2025-07-27 10:48:18 -0700

    removed code from delete_data

--- delete_data.py
+++ delete_data.py
@@ -21,7 +21,7 @@
 from arcgis.gis import GIS
 from arcgis.features import FeatureLayer
 
-gis = GIS("https://intern-hackathon.maps.arcgis.com", "sambala_intern_hackathon", "Thisispizza@2477")
+gis = GIS("https://intern-hackathon.maps.arcgis.com", "sambala_intern_hackathon", "<your_password_here>")
 feature_layer_url = "https://services8.arcgis.com/LLNIdHmmdjO2qQ5q/arcgis/rest/services/Detected_flytwo/FeatureServer/0"
 layer = FeatureLayer(feature_layer_url)
 
